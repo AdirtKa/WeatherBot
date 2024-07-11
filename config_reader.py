c512e7cb0cfeddd55dfce732a7cf764014b85240
Daniil <AdirtKa@yandex.ru>
2024-07-11 20:50:39 +1000

    Update config_reader.py

--- config_reader.py
+++ config_reader.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     bot_token: SecretStr
     weather_token: SecretStr
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=None)
 
 
 config: Settings = Settings()
